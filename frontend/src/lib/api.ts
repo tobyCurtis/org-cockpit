823e07a320e8d8d8a030f917f9ff2597ec2a6a8c
Toby Curtis <me@toby.land>
2025-12-07 11:28:22 -0800

    Update readme after store and type cleaneup

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,30 +4,8 @@ declare global {
   }
 }
 
-
-export interface SfOrg {
-  alias?: string;
-  username: string;
-  instanceUrl: string;
-  connectedStatus?: string;
-  isDevHub?: boolean;
-  isDefaultUsername?: boolean;
-  defaultMarker?: string;
-  lastUsed?: string;
-}
-
-export interface OrgListResult {
-  status: number;
-  result: {
-    scratchOrgs: SfOrg[];
-    nonScratchOrgs: SfOrg[];
-    sandboxes: SfOrg[];
-    devHubs: SfOrg[];
-  };
-  warnings: string[];
-}
-
-export type AddOrgMode = 'production' | 'sandbox' | 'custom';
+import type { AddOrgMode, OrgListResult } from "./api/types";
+export type { SfOrg, OrgListResult, AddOrgMode } from "./api/types";
 
 export async function getAuthorizedOrgs(): Promise<OrgListResult> {
   return await window.electron.ipcRenderer.invoke('get-orgs');
--- frontend/src/lib/api/types.ts
+++ frontend/src/lib/api/types.ts
@@ -2,7 +2,22 @@ export interface SfOrg {
   alias?: string;
   username: string;
   instanceUrl: string;
-  isDevHub?: boolean;
   connectedStatus?: string;
+  isDevHub?: boolean;
+  isDefaultUsername?: boolean;
+  defaultMarker?: string;
   lastUsed?: string;
 }
+
+export interface OrgListResult {
+  status: number;
+  result: {
+    scratchOrgs: SfOrg[];
+    nonScratchOrgs: SfOrg[];
+    sandboxes: SfOrg[];
+    devHubs: SfOrg[];
+  };
+  warnings: string[];
+}
+
+export type AddOrgMode = "production" | "sandbox" | "custom";
